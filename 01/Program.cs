08475ede9f22faabe95912c618eebff38fc8e40d
Anders Moldin <anders.moldin@lakemedelsverket.se>
2020-12-01 13:04:18 +0100

    Solution to day 1

--- 01/Program.cs
+++ 01/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 /// <summary>
 /// Finds the two addends that adds up to 2020
 /// </summary>
-(int First, int Second) Find2020(List<int> addends)
+(int First, int Second) FindTwoAddends(List<int> addends)
 {
     for (int first = 0; first < addends.Count; first++)
     {
@@ -22,10 +22,36 @@ using System.Linq;
     throw new Exception("Could not find addends that equals 2020.");
 }
 
+/// <summary>
+/// Finds the two addends that adds up to 2020
+/// </summary>
+(int First, int Second, int Third) FindThreeAddends(List<int> addends)
+{
+    for (int first = 0; first < addends.Count; first++)
+    {
+        for (int second = 0; second < first + 1; second++)
+        {
+            for (int third = 0; third < second + 1; third++)
+            {
+                if (addends[first] + addends[second] + addends[third] == 2020)
+                {
+                    return (addends[first], addends[second], addends[third]);
+                }
+            }
+        }
+    }
+
+    throw new Exception("Could not find addends that equals 2020.");
+}
+
+// Example data and input data from https://adventofcode.com/2020/day/1
 //var expenseReport = File.ReadLines("exampleInput.txt").Select(int.Parse).ToList();
 var expenseReport = File.ReadLines("input.txt").Select(int.Parse).ToList();
 
-var addends = Find2020(expenseReport);
+//Run methods to find addends of 2020
+var twoAddends = FindTwoAddends(expenseReport);
+var threeAddends = FindThreeAddends(expenseReport);
 
-Console.WriteLine($"Part 1: {addends.First * addends.Second}");
-Console.WriteLine($"Part 2: {addends.First * addends.Second}");
+//Print results
+Console.WriteLine($"Part 1: {twoAddends.First * twoAddends.Second}");
+Console.WriteLine($"Part 2: {threeAddends.First * threeAddends.Second * threeAddends.Third}");
