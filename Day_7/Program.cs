35f7019fe48c73242836276ade06b2be3bd9a608
andersmoldin <anders@moldin.com>
2020-12-07 07:29:59 +0100

    * exampleInput.txt: Initial code for Day 7
    * input.txt:
    * Program.cs:

--- Day_7/Program.cs
+++ Day_7/Program.cs
@@ -1,3 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-Console.WriteLine("Hello world");
+//var input = File.ReadLines("input.txt");
+var input = File.ReadLines("exampleInput.txt");
+
+Console.WriteLine($"Part 1: {Part1(input)}");
+
+int Part1(IEnumerable<string> allRules)
+{
+    do
+    {
+
+    } while (true);
+
+    return int.MinValue;
+}
