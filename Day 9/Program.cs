ad9c3472eb69fe32ca1a46d73f1de4de4851355d
andersmoldin <anders@moldin.com>
2020-12-09 08:07:55 +0100

    * input.txt: * Program.cs: * Program.cs: * Day 9.csproj: * Day_7.csproj: * exampleInput.txt: * exampleInput2.txt:
    * AdventOfCode2020.sln: Initial work for Day 9

---
+++ Day 9/Program.cs
@@ -0,0 +1,55 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+//var input = File.ReadLines("input.txt").Select(int.Parse).ToArray();
+var input = File.ReadLines("exampleInput.txt").Select(int.Parse).ToArray();
+
+//Print results
+Console.WriteLine($"Part 1: {Part1(input, 5)}");
+//Console.WriteLine($"Part 2: {Part2(input)}");
+
+
+int Part1(int[] xmasCode, int preamble)
+{
+	int? notSumOfAnyPreviousInPreamble = null;
+
+	var validValues = new HashSet<int>();
+
+	for (int currentValue = preamble; currentValue < xmasCode.Length/* - preamble*/; currentValue++)
+	{
+		for (int firstAddend = currentValue - preamble; firstAddend < preamble; firstAddend++)
+		{
+            if (notSumOfAnyPreviousInPreamble.HasValue)
+            {
+				continue;
+            }
+
+            for (int secondAddend = firstAddend + 1; secondAddend < preamble; secondAddend++)
+            {
+				if (xmasCode[firstAddend] + xmasCode[secondAddend] == xmasCode[currentValue])
+                {
+					notSumOfAnyPreviousInPreamble = xmasCode[currentValue];
+
+					validValues.Add(xmasCode[currentValue]);
+
+					continue;
+                }
+            }
+		}
+
+        if (!notSumOfAnyPreviousInPreamble.HasValue)
+        {
+			return xmasCode[currentValue];
+        }
+
+		notSumOfAnyPreviousInPreamble = null;
+	}
+
+	var hej1 = validValues.Except(xmasCode).ToList();
+	var hej2 = xmasCode.Except(validValues).ToList();
+
+	return int.MinValue;
+}
