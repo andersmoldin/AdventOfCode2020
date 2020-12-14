3c452864713154d20f5cf1a6eabef650329cb54a
andersmoldin <anders@moldin.com>
2020-12-14 20:52:33 +0100

    * input.txt: * Program.cs: * Day 11.csproj: * exampleInput.txt:
    * Program.cs: Solved Day 11, Part 1

--- Day 10/Program.cs
+++ Day 10/Program.cs
@@ -4,17 +4,32 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-var input = File.ReadLines("input.txt").Select(int.Parse).ToList();
-//var input = File.ReadLines("exampleInput.txt").Select(int.Parse).ToList();
+var input = File.ReadLines("input.txt").Select(int.Parse).OrderBy(a => a).ToArray();
+//var input = File.ReadLines("exampleInput.txt").Select(int.Parse).OrderBy(a => a).ToArray();
+//var input = File.ReadLines("exampleInput2.txt").Select(int.Parse).OrderBy(a => a).ToArray();
 
 // Print answers
 Console.WriteLine(Part1(input));
 Console.WriteLine(Part2(input));
 
 // Part 1
-int Part1(IEnumerable<int> adapters)
+int Part1(int[] adapters)
 {
-    return int.MinValue;
+    var differences = new Dictionary<int, int> { { 1, 0 }, { 2, 0 }, { 3, 1 } };
+
+    for (int i = 0; i < adapters.Length; i++)
+    {
+        if (i == 0)
+        {
+            differences[Math.Abs(0 - adapters[i])]++;
+        }
+        else
+        {
+            differences[Math.Abs(adapters[i - 1] - adapters[i])]++;
+        }
+    }
+
+    return differences.Where(d => d.Value > 0).Select(d => d.Value).Aggregate((a, b) => a * b);
 }
 
 // Part 2
--- Day 11/Program.cs
+++ Day 11/Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace Day_11
+var input = File.ReadLines("input.txt").Select(l => l.ToCharArray()).ToArray();
+//var input = File.ReadLines("exampleInput.txt").Select(l => l.ToCharArray()).ToArray();
+
+// Print answers
+Console.WriteLine(Part1(input));
+Console.WriteLine(Part2(input));
+
+// Part 1
+int Part1(char[][] layout)
 {
-    class Program
+    var positionsToChange = new Dictionary<(int X, int Y), char>();
+
+    do
     {
-        static void Main(string[] args)
+        positionsToChange = GetPositionsToChange(layout);
+        layout = ChangePositions(positionsToChange, layout);
+    } while (positionsToChange.Count > 0);
+
+    return layout.SelectMany(s => s).Count(s => s == '#');
+}
+
+// Part 2
+int Part2(char[][] adapters)
+{
+    return int.MinValue;
+}
+
+Dictionary<(int X, int Y), char> GetPositionsToChange(char[][] layout)
+{
+    var positionsToChange = new Dictionary<(int X, int Y), char>();
+
+    for (int row = 0; row < layout.Length; row++)
+    {
+        for (int column = 0; column < layout[row].Length; column++)
         {
-            Console.WriteLine("Hello World!");
+            var adjacentSeats = GetAdjacentSeats(layout, row, column);
+
+            switch (layout[row][column])
+            {
+                case 'L':
+                    if (!adjacentSeats.Any(s => s == '#'))
+                    {
+                        positionsToChange.Add((row, column), '#');
+                    }
+                    break;
+                case '#':
+                    if (adjacentSeats.Count(s => s == '#') >= 4)
+                    {
+                        positionsToChange.Add((row, column), 'L');
+                    }
+                    break;
+                case '.':
+                    break;
+                default:
+                    throw new Exception($"Unknown character in positin ({row},{column})!");
+            }
         }
     }
+
+    return positionsToChange;
+}
+
+char[] GetAdjacentSeats(char[][] layout, int row, int column)
+{
+    var adjacentSeats = "";
+
+    for (int i = 1; i >= -1; i--)
+    {
+        for (int j = 1; j >= -1; j--)
+        {
+            if (row + i >= 0 && column + j >= 0 && row + i < layout.Length && column + j < layout[0].Length && !(i == 0 && j == 0))
+            {
+                adjacentSeats += layout[row + i][column + j];
+            }
+        }
+    }
+
+    return adjacentSeats.ToCharArray();
+}
+
+char[][] ChangePositions(Dictionary<(int X, int Y), char> positionsToChange, char[][] layout)
+{
+    for (int row = 0; row < layout.Length; row++)
+    {
+        for (int column = 0; column < layout[row].Length; column++)
+        {
+            if (positionsToChange.ContainsKey((row, column)))
+            {
+                layout[row][column] = positionsToChange[(row, column)];
+            }
+        }
+    }
+
+    return layout;
 }
